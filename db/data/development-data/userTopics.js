a5c77fdac4da0fea323b55c9568cb7ad43bb3b39
ofeore <ofeore@gmail.com>
2026-01-31 15:05:01 +0000

    Create test and dev data for user_topics seeding

---
+++ db/data/development-data/userTopics.js
@@ -0,0 +1,17 @@
+module.exports = [
+  { username: "tickle122", topic: "coding" },
+  { username: "tickle122", topic: "football" },
+
+  { username: "grumpy19", topic: "football" },
+
+  { username: "happyamy2016", topic: "cooking" },
+  { username: "happyamy2016", topic: "coding" },
+
+  { username: "cooljmessy", topic: "coding" },
+
+  { username: "weegembump", topic: "football" },
+  { username: "weegembump", topic: "cooking" },
+
+  { username: "jessjelly", topic: "coding" },
+  { username: "jessjelly", topic: "cooking" },
+];
---
+++ db/data/test-data/userTopics.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { username: "butter_bridge", topic: "mitch" },
+  { username: "butter_bridge", topic: "cats" },
+
+  { username: "icellusedkars", topic: "paper" },
+
+  { username: "rogersop", topic: "paper" },
+
+  { username: "lurker", topic: "cats" },
+];
