c67570624b45bfeb89302d52cd30fe8dedb9dd20
ofeore <ofeore@gmail.com>
2026-02-04 14:24:32 +0000

    Add /api/articles endpoint to app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const topicsRouter = require("./routes/topics.routes");
+const articlesRouter = require("./routes/articles.routes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/topics", topicsRouter);
+app.use("/api/articles", articlesRouter);
 
 module.exports = app;
