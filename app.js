5b3df11bd8d4f30be5189eb3930e5cb435716971
ofeore <ofeore@gmail.com>
2026-02-06 12:14:17 +0000

    Remove sprint instructions from app.js

--- app.js
+++ app.js
@@ -29,21 +29,3 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
-
-// Description
-// Should:
-
-// be available on /api/articles/:article_id/comments.
-// get all comments for an article.
-// Responds with:
-
-// an object with the key of comments and the value of an array of comments for the given article_id. Each comment should have the following properties:
-// comment_id
-// votes
-// created_at
-// author
-// body
-// article_id
-// Comments should be served with the most recent comments first.
-
-// Consider what errors could occur with this endpoint, and make sure to test for them.
